f6e119c2fb75aba397139ec64d3678f113ee80e3
steflogan23 <steflogan23@gmail.com>
2023-12-12 16:22:55 -0600

    Week 2 REST

--- pet-store/src/main/java/pet/store/entity/PetStore.java
+++ pet-store/src/main/java/pet/store/entity/PetStore.java
@@ -23,7 +23,8 @@ public class PetStore {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	
-	private long petStoreId;
+	private Long petStoreId;
+	private String petStoreName;
 	private String petStoreAddress;
 	private String petStoreCity;
 	private String petStoreState;
